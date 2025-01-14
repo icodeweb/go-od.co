43c0f6747023bd1bc3421e032c84770e4d4afe0a
Ibrahim Ismail <ebrahemesmael@gmail.com>
2025-01-14 07:04:01 +0200

    added first work to the array and home page 50% done

---
+++ works.js
@@ -0,0 +1,71 @@
+let works = [
+    {
+        title: "Intra",
+        client: "Intra",
+        year: 2020,
+        services: "Brand, Design, Website Design and Development",
+        info: "In 2020 we worked with the team at Intra to create an iconic mark and visual presence for the much-loved cafe in Campbell. The brand has a hero mark and a simple sans serif wordmark. The mark in its simplest form is the tittle for the i, however, when shown in isolation it transforms into 'the eye', which represents the idea of the more I interact the more I see. The more I see the more I know. The more I know, the more I know Intra. The eye is the guide it leads the people in the direction of good food, good drinks, and a warming community. Acting as a mascot, the eye is the hero feature for the store's signage and the website homepage. A recognisable mark that plays a part in telling this brand's story.",
+        www: "https://intracbr.com.au",
+        instagram: "@intracbr",
+        instagramLink: "https://www.instagram.com/intracbr/",
+        media: [
+            {
+                name: "logo.jpg",
+                src: "/assets/works/intra/eye-intra-10-scaled.jpg",
+                format: "jpg"
+            },
+            {
+                name: "oyster-poster.jpg",
+                src: "/assets/works/intra/oyster-poster.jpg",
+                format: "jpg"
+            }, {
+                name: "menu.gif",
+                src: "https://player.vimeo.com/video/727600899?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "intra-x-para.gif",
+                src: "https://player.vimeo.com/video/727602998?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "lowercase-eye.gif",
+                src: "https://player.vimeo.com/video/728674089?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "collaboration_tees.jpg",
+                src: "/assets/works/intra/Intra_Tshirt.jpg",
+                format: "jpg"
+            }, {
+                name: "spiced-nuts.gif",
+                src: "https://player.vimeo.com/video/727622103?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "paranormal-menu.jpg",
+                src: "/assets/works/intra/parax-intra-11-1-scaled.jpg",
+                format: "jpg"
+            }, {
+                name: "intra-signage.jpg",
+                src: "/assets/works/intra/intra-signage.jpg",
+                format: "jpg"
+            }, {
+                name: "temperada-takeover.gif",
+                src: "https://player.vimeo.com/video/731212290?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "website.gif",
+                src: "https://player.vimeo.com/video/731218598?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "nomadic-poster.jpg",
+                src: "/assets/works/intra/nomadicposter-intra-12-1.jpg",
+                format: "jpg"
+            }, {
+                name: "nomadic-flag.jpg",
+                src: "/assets/works/intra/nomadicflag-intra.jpg",
+                format: "jpg"
+            }
+
+        ],
+    }
+]
+
+
