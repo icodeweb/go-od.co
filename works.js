7e00779f614c41dd6817d491afea16356b1a4a85
Ibrahim Ismail <ebrahemesmael@gmail.com>
2025-01-15 04:36:31 +0200

    more works

--- works.js
+++ works.js
@@ -65,7 +65,251 @@ let works = [
             }
 
         ],
+    }, {
+        title: "Kundalini",
+        client: "Kundalini",
+        year: 2021,
+        services: "Brand, Design, Website Design and Development, Photography",
+        info: "Kundalini, meaning 'divine female energy'. We expanded on this to be more inclusive and gender diverse. Looking at energy as the idea of radiating, glowing and expanding and pairing that with their practice of curating. Kundalini, curating your radiance, was the foundation of the brand which came to life with a range of abstract hair- based patterns, custom iconography and an evolving colour palette.The basis for the colour palette was natural hair colours, which were then integrated with vibrant and moody tones to create depth.The colour palette consistently ties back to hair in abstracted forms and tones, while avoiding literal representation. The wordmark is a custom typemark.We looked to blend the elegance of a display serif with the structure of a solid san - serif.This created a number of interesting interactions between the letterforms.We paired this mark with GT America for its clean legibility that complimented the more expensive patterns and imagery. This all came to life with a vibrant editorial.Where we invited staff and clients to participate.Pairing extravagant styling and set backdrops with real people.No models, just people having a great time.Not only did this help position the brand in a new light it ignited the team’s creative energy to move into this new chapter.",
+        www: "https://kundalinihair.com.au",
+        instagram: "kundalinihair",
+        instagramLink: "https://www.instagram.com/kundalinihair/",
+        media: [
+            {
+                name: "kundalini-brand.jpg",
+                src: "/assets/works/Kundalini/Post.jpg",
+                format: "jpg"
+            }, {
+                name: "brand-guidelines.jpg",
+                src: "/assets/works/Kundalini/brand.jpg",
+                format: "jpg"
+            }, {
+                name: "posters.jpg",
+                src: "/assets/works/Kundalini/",
+                format: "jpg"
+            }, {
+                name: "mark.gif",
+                src: "https://player.vimeo.com/video/736757875?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "colour-palette.gif",
+                src: "https://player.vimeo.com/video/736759104?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "curly-card.jpg",
+                src: "/assets/works/Kundalini/curlycard.jpg",
+                format: "jpg"
+            }, {
+                name: "website-home.mp4",
+                src: "https://player.vimeo.com/video/723602484?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "mp4"
+            }, {
+                name: "pattern-cards.gif",
+                src: "https://player.vimeo.com/video/727283278?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "five-colour-sets.jpg",
+                src: "/assets/works/Kundalini/colours.jpg",
+                format: "jpg"
+            }, {
+                name: "social-feed.jpg",
+                src: "/assets/works/Kundalini/socialfeed.jpg",
+                format: "jpg"
+            }, {
+                name: "product-group.jpg",
+                src: "/assets/works/Kundalini/Product2.jpg",
+                format: "jpg"
+            }, {
+                name: "icon-sets.jpg",
+                src: "/assets/works/Kundalini/customicon.jpg",
+                format: "jpg"
+            }, {
+                name: "product-page.mp4",
+                src: "https://player.vimeo.com/video/723618972?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "mp4"
+            }, {
+                name: "staff-moni.jpg",
+                src: "/assets/works/Kundalini/Staff.jpg",
+                format: "jpg"
+            }, {
+                name: "mobile-website.mp4",
+                src: "https://player.vimeo.com/video/723617528?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "mp4"
+            }, {
+                name: "website-menu.jpg",
+                src: "/assets/works/Kundalini/webmenu.jpg",
+                format: "jpg"
+            }, {
+                name: "editorial-product.jpg",
+                src: "/assets/works/Kundalini/eduProduct.jpg",
+                format: "jpg"
+            }, {
+                name: "wavy-card.jpg",
+                src: "/assets/works/Kundalini/cardwavy.jpg",
+                format: "jpg"
+            }, {
+                name: "product.jpg",
+                src: "/assets/works/Kundalini/Product1.jpg",
+                format: "jpg"
+            }, {
+                name: "type.jpg",
+                src: "/assets/works/Kundalini/font.jpg",
+                format: "jpg"
+            }, {
+                name: "product-group.jpg",
+                src: "/assets/works/Kundalini/Product3.jpg",
+                format: "jpg"
+            }, {
+                name: "print-collateral.jpg",
+                src: "/assets/works/Kundalini/messymock.jpg",
+                format: "jpg"
+            },]
+    }, {
+        title: "Lab",
+        client: "Lab",
+        year: 2022,
+        services: "Brand Strategy, Brand, Website Design, Content Marketing",
+        info: "In an industry saturated with neutrals and pinks, our aim was to create a brand that wasn't restricted by beauty standards. We created a mark that can mould and change, be minimal or playful, colourful or paired back. Built from five simple shapes: two circles and three rectangles. This then builds into three icons that represent the brand's core offering. The colours are bold and nostalgic and are used together or individually as punchy backgrounds. We introduced a quirky title font with curves that resemble lashes and brows to command the attention of the layouts so the logo doesn't need to do all the work. We continue to work with the client on implementing this new approach across their social media content, website and packaging.",
+        www: "https://www.the-lab.com.au",
+        credits: [{
+            credit: "Design",
+            name: "Benny Grocott"
+        }, {
+            credit: "Strategy",
+            name: "Jessica Di Scipio"
+        }, {
+            credit: "Strategy",
+            name: "Bronte Morgan"
+        }, {
+            credit: "Design",
+            name: "Declan Florence"
+        }],
+        instagram: "@itsthelab",
+        instagramLink: "https://www.instagram.com/itsthelab/",
+        media: [
+            {
+                name: "logo.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "web-home.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "photography.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "lab-moving.mp4",
+                src: "https://player.vimeo.com/video/727648329?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "mp4"
+            }, {
+                name: "web-shop.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "manifesto.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "icons.gif",
+                src: "https://player.vimeo.com/video/735276117?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "social-feed.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "box.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "moving-images.gif",
+                src: "https://player.vimeo.com/video/732326365?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "colour.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "stories.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "reel.mp4",
+                src: "https://player.vimeo.com/video/732329003?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "mp4"
+            }, {
+                name: "patterns.gif",
+                src: "https://player.vimeo.com/video/731232421?h=98633cdd86&badge=0&autoplay=1&loop=1&autopause=0&muted=1&background=1&player_id=0&app_id=58479",
+                format: "gif"
+            }, {
+                name: "object-sans.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "nurture-display.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "product-photography.jpg",
+                src: "",
+                format: "jpg"
+            }, {
+                name: "web-blog.jpg",
+                src: "",
+                format: "jpg"
+            }]
     }
 ]
 
 
+
+
+
+const viewToggle = document.querySelector(".viewToggle")
+const dropdown = document.querySelector(".dropdown")
+
+
+viewToggle.addEventListener("click", () => {
+    if (dropdown.style.display == "none" || dropdown.style.display == "") {
+        dropdown.style.display = "block"
+    }
+
+})
+
+viewToggle.addEventListener("mouseleave", () => {
+    if (dropdown.style.display == "block") {
+        dropdown.style.display = "none"
+    }
+})
+
+let spanTexts = [];
+for (let index = 0; index < 1; index++) {
+    document.querySelectorAll(".title span").forEach((span) => {
+        spanTexts.push(span.textContent)
+        console.log(spanTexts);
+    });
+}
+
+
+[
+    "logo.jpg",
+    "web-home.jpg",
+    "photography.jpg",
+    "lab-moving.mp4",
+    "web-shop.jpg",
+    "manifesto.jpg",
+    "icons.gif",
+    "social-feed.jpg",
+    "box.jpg",
+    "moving-images.gif",
+    "colour.jpg",
+    "stories.jpg",
+    "reel.mp4",
+    "patterns.gif",
+    "object-sans.jpg",
+    "nurture-display.jpg",
+    "product-photography.jpg",
+    "web-blog.jpg"
+]
